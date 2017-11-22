83fb78df49d079272e769da1011c78298a202acd
Thong <Thong@THONGMAP95>
2017-11-22 08:55:34 +0700

    Add project files.

---
+++ DVWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVWeb.Startup))]
+namespace DVWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
